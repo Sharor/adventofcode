bc096b56f305013f7410b535d6d501b5bd30bef2
Sharor <johannes_christensen@msn.com>
2017-12-02 19:33:58 +0100

    wip

---
+++ day1/day1/AdvancedCaptchaEvaluator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace day1
+{
+	public class AdvancedCaptchaEvaluator
+	{
+		public int EvaluateCaptcha(string puzzle)
+		{
+			return CheckConsecutiveDigits(puzzle); 
+		}
+
+		public int CheckConsecutiveDigits(string puzzle)
+		{
+			int consecutiveValue = 0;
+			int midway = puzzle.Length / 2; 
+
+			for (int i = 0; i < puzzle.Length; i++)
+			{
+				if (i != puzzle.Length)
+				{
+					if(i+midway>puzzle.Length-1)
+					{
+						if(puzzle[i]==puzzle[puzzle.Length-i])
+							consecutiveValue += (int)Char.GetNumericValue(puzzle[i]);
+					}
+					else
+					{
+						if(puzzle[i]==puzzle[i+midway])
+						consecutiveValue += (int)Char.GetNumericValue(puzzle[i]);
+					}
+						
+				}
+					
+			}
+			return consecutiveValue;
+		}
+	}
+}
---
+++ day1/day1test/AdvancedPuzzleTests.cs
@@ -0,0 +1,56 @@
+﻿using day1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace day1test
+{
+    public class AdvancedPuzzleTests
+    {
+		[Fact]
+		public void PuzzleExample1()
+		{
+			string puzzle = "1212";
+			AdvancedCaptchaEvaluator captcha = new AdvancedCaptchaEvaluator();
+			int notAHuman = captcha.EvaluateCaptcha(puzzle);
+			Assert.Equal(notAHuman, 6);
+		}
+
+		[Fact]
+		public void PuzzleExample2()
+		{
+			string puzzle = "1221";
+			AdvancedCaptchaEvaluator captcha = new AdvancedCaptchaEvaluator();
+			int notAHuman = captcha.EvaluateCaptcha(puzzle);
+			Assert.Equal(notAHuman, 0);
+		}
+
+		[Fact]
+		public void PuzzleExample3()
+		{
+			string puzzle = "123425";
+			AdvancedCaptchaEvaluator captcha = new AdvancedCaptchaEvaluator();
+			int notAHuman = captcha.EvaluateCaptcha(puzzle);
+			Assert.Equal(notAHuman, 4);
+		}
+
+		[Fact]
+		public void PuzzleExample4()
+		{
+			string puzzle = "123123";
+			AdvancedCaptchaEvaluator captcha = new AdvancedCaptchaEvaluator();
+			int notAHuman = captcha.EvaluateCaptcha(puzzle);
+			Assert.Equal(notAHuman, 12);
+		}
+
+		[Fact]
+		public void PuzzleExample5()
+		{
+			string puzzle = "12131415";
+			AdvancedCaptchaEvaluator captcha = new AdvancedCaptchaEvaluator();
+			int notAHuman = captcha.EvaluateCaptcha(puzzle);
+			Assert.Equal(notAHuman, 4);
+		}
+	}
+}
