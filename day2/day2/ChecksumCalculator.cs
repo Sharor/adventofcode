8da33c1082d0133d901b9b989f910642705953fe
Sharor <johannes_christensen@msn.com>
2017-12-02 19:08:47 +0100

    #1 minor - this closes #1 for another gold star, huzzah!

---
+++ day2/day2/ChecksumCalculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace day2
+{
+	public class ChecksumCalculator
+	{
+		public int RowChecksum(string puzzle)
+		{
+			string[] splitNumbers = puzzle.Split('\t');
+			int[] numbers = Array.ConvertAll(splitNumbers, int.Parse);
+
+			int max = 0;
+			int min = numbers[0]; 
+			foreach(int number in numbers)
+			{
+				if (number < min)
+					min = number;
+				if (number > max)
+					max = number;					
+			}
+			return max - min;
+		}
+
+		public int TotalChecksum(string puzzle)
+		{
+			int total = 0;
+			string[] splitLines = puzzle.Split('\n');
+			foreach(string line in splitLines)
+			{
+				total += RowChecksum(line); 
+			}
+			return total;
+		}
+	}
+}
---
+++ day2/day2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			ChecksumCalculator captcha = new ChecksumCalculator();
+			Console.WriteLine("Captcha for the given puzzle is: " + captcha.TotalChecksum(new Puzzle().puzzle));
+			Console.Read();
+		}
+	}
+}
